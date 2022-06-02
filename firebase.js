6996dcf9df800b5a45722898eca2a0cd644d8ee3
Danilo Lekovic <danilo_lekovic@sfu.ca>
2022-06-02 11:46:30 -0700

    Fixed Firebase connection; created blank login comp

--- firebase.js
+++ firebase.js
@@ -9,18 +9,25 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREABSE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-}
+  apiKey: '${process.env.FIREBASE_API_KEY',
+  authDomain: '${process.env.FIREBASE_AUTH_DOMAIN',
+  databaseURL: '${process.env.FIREBASE_DATABASE_URL',
+  projectId: '${process.env.FIREBASE_PROJECT_ID',
+  storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET',
+  messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID',
+  appId: '${process.env.FIREBASE_APP_ID',
+  measurementId: '${process.env.FIREBASE_MEASUREMENT_ID'
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+let analytics;
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app)
+
+if (app.name && typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+}
+
+export default { app, auth, analytics }
